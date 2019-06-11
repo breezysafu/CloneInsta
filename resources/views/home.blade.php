8dc3f4090e38072faa9eefc818840a4e77d6f26f
breezysafu <breezy.safu@gmail.com>
2019-06-11 09:00:56 +0545

    tinker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,28 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://yt3.ggpht.com/a/AGF-l7_sGUrG_alh-mdTIDmdcaw1GanuPL2PxVajjQ=s48-mo-c-c0xffffffff-rj-k-no" style="width:100px;height:100px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5 pl-10">
+            <div><h1>CloneInsta</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>22</strong>posts</div>
+                <div class="pr-5"><strong>1k</strong>followers</div>
+                <div class="pr-5"><strong>369</strong>following</div>
             </div>
+            <div class="pt-4 font-weight-bold">cloneinsta.org</div>
+            <div>This is the community of millions of people where people get famous by their modelling.</div>
+            <div><a href="#">www.cloneinsta.org</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4"><img src="https://rukminim1.flixcart.com/image/832/832/jd7p18w0/poster/h/9/p/medium-gigi-hadid-models-hd-wall-poster-celeb1421-original-imaek7e8nryhneb3.jpeg?q=70" class="w-100"></div>
+        <div class="col-4"><img src="https://rukminim1.flixcart.com/image/832/832/jd7p18w0/poster/h/9/p/medium-gigi-hadid-models-hd-wall-poster-celeb1421-original-imaek7e8nryhneb3.jpeg?q=70" class="w-100"></div>
+        <div class="col-4"><img src="https://rukminim1.flixcart.com/image/832/832/jd7p18w0/poster/h/9/p/medium-gigi-hadid-models-hd-wall-poster-celeb1421-original-imaek7e8nryhneb3.jpeg?q=70" class="w-100"></div>
+    </div>
+
 </div>
 @endsection
